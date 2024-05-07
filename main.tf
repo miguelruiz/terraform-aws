5c4e8a10a9768da705ad3a54643b439916ac8a11
Miguel Ruiz Manzano <mruiz@linux.com>
2024-05-07 14:47:16 -0400

    Removed gateways from vpc module setup.

--- main.tf
+++ main.tf
@@ -24,9 +24,6 @@ module "blog_vpc" {
   azs             = ["sa-east-1a","sa-east-1b","sa-east-1c"]
   public_subnets  = ["10.0.101.0/24","10.0.102.0/24","10.0.103.0/24"]
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
-
   tags= {
     Terraform = "true"
     Environment = "dev"
