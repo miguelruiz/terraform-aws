037a00c1115d8a294d5f559c1fce07c46106dbb1
Miguel Ruiz Manzano <mruiz@linux.com>
2024-05-07 16:21:52 -0400

    Added missing definition for targets.

--- main.tf
+++ main.tf
@@ -31,15 +31,12 @@ module "blog_vpc" {
 }
 
 module "alb" {
-  source = "terraform-aws-modules/alb/aws"
-
-  name    = "blog-alb"
-
-  load_balancer_type = "application"
-
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
+  source              = "terraform-aws-modules/alb/aws"
+  name                = "blog-alb"
+  load_balancer_type  = "application"
+  vpc_id              = module.blog_vpc.vpc_id
+  subnets             = module.blog_vpc.public_subnets
+  security_groups     = module.blog_sg.security_group_id
 
   target_groups = [
     {
@@ -47,8 +44,7 @@ module "alb" {
       backend_protocol  = "HTTP"
       backend_port      = 80
       target_type       = "instance"
-      
-      targets {
+      targets = {
         my_target = {
           target_id = aws_instance.blog.id
           port      = 80
