26c27a930a36133a320db2ebac20d019427b432b
Miguel Ruiz Manzano <mruiz@linux.com>
2024-05-08 18:04:41 -0400

    Refactoring to use variables.

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,21 +11,20 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner]
 }
 
 module "blog_vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.8.1"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name            = var.environment.name
+  cidr            = "${var.environment.network_prefix}.0.0/16"
   azs             = ["sa-east-1a","sa-east-1b","sa-east-1c"]
-  public_subnets  = ["10.0.101.0/24","10.0.102.0/24","10.0.103.0/24"]
-
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24","${var.environment.network_prefix}.102.0/24",${var.environment.network_prefix}.103.0/24"]
   tags= {
     Terraform = "true"
-    Environment = "dev"
+    Environment = var.environment.name
   } 
 }
 
@@ -52,7 +51,7 @@ module "blog_alb" {
   }
 
   tags = {
-    Environment = "dev"
+    Environment = var.environment.name
   }
 
 }
@@ -61,7 +60,7 @@ module "blog_sg" {
   source      = "terraform-aws-modules/security-group/aws"
   version     = "5.1.2"
 
-  name                = "blog_sg"
+  name                = "${var.environment.name}-blog"
   description         = "Allow http and https in. Allow everything out"
   vpc_id              = module.blog_vpc.vpc_id
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
@@ -74,9 +73,9 @@ module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "7.4.1"
 
-  name = "blog_asg"
-  min_size = 1
-  max_size = 2
+  name      = "${var.environment.name}-blog_asg"
+  min_size  = var.asg_min_size
+  max_size  = var.asg_max_size
   
   vpc_zone_identifier = module.blog_vpc.public_subnets
   security_groups     = [module.blog_sg.security_group_id]
